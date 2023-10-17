d0316e80bd44df6801712210c5a7ab3acedbb102
hiiiiiilim <vnfwlq07@naver.com>
2023-10-17 16:46:51 +0900

    .

--- chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
+++ chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
@@ -10,6 +10,13 @@ import java.sql.SQLException;
 public class DBConnection {
 
 	public static void main(String[] args) {
+		//selectBank();
+		//selectKhcafe();
+		//selectIf();
+		selectwhile();
+	}
+	
+	static void selectBank() {
 		// 1. 드라이버 연결: Oracle JDBC 드라이버 클래스 이름
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		//2. 오라클 내 컴퓨터 연결:데이터베이스 연결정보
@@ -46,5 +53,87 @@ public class DBConnection {
 			e.printStackTrace();
 		}
 	}
-
+	
+	static void selectKhcafe() {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khcafe";
+		String password = "khcafe";
+		Connection con = null;
+		
+		try {
+			con =DriverManager.getConnection(url, user, password);
+			System.out.println("db연결 성공");
+			
+			String selectQuery = "select * from menu where price >= 2000 order by price desc";
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			ResultSet result = selectState.executeQuery();
+			while(result.next()) {
+				int menuId = result.getInt("menu_id");
+				String menuName = result.getString("menu_name");
+				double price = result.getDouble("price");
+				System.out.println(menuId + " "+menuName+" "+price);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static void selectIf() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khbank";
+		String password = "khbank";
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			//where절 사용해서 조건 추가
+			String selectQuery = "SELECT * FROM BANK WHERE account_id = ?";
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			
+			/*
+			String[] targetAN= {"1234567890", "5555666777"};
+			selectState.setString(1, targetAN[0]);
+			selectState.setString(2, targetAN[1]);
+			*/
+			
+			// 여기에 원하는 조건의 account_id설정
+			int targetAID = 1;
+			
+			//조건설정
+			selectState.setInt(1, targetAID); //1은 자리값, 열에서 몇번째 열에서 검색할 것인지 예를 들어 1 account_id, 2 account_number
+			//targetAID where 절의 조건을 설정해주는 함수. 변경해주면 where절뒤에 조건이 변경이 된다. 
+			
+			ResultSet result = selectState.executeQuery();
+			
+			//값존재여부확인
+			if(result.next()) {
+				int a = result.getInt("account_id");
+				String b = result.getString("account_number");
+				System.out.println("ACCOUNT_ID: "+a+" ACCOUNT_NUMBER: "+b);
+			}else {
+				System.out.println("...");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	static void selectwhile() {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khstore";
+		String passwoed = "khstore";
+		
+		Connection con= null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, passwoed);
+			System.out.println("데이터베이스연결성공");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
