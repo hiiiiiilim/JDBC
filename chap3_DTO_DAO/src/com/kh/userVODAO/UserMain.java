c44b9d468bf93e12d7081667c7799b571caa1987
hiiiiiilim <vnfwlq07@naver.com>
2023-10-23 15:22:21 +0900

    .

--- chap3_DTO_DAO/src/com/kh/userVODAO/UserMain.java
+++ chap3_DTO_DAO/src/com/kh/userVODAO/UserMain.java
@@ -2,6 +2,8 @@ package com.kh.userVODAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
@@ -12,10 +14,70 @@ public class UserMain {
 	public static void main(String[] args) {
 		UserMain um = new UserMain();
 		
-		um.selectAll();
-	}	
+		um.selectScanner();
+	}
+	
 
-	public void selectScanner
+
+	public void selectScanner() {
+		// 1. DB연결 url, username, password
+		String jdbcURL = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbUserName = "khcafe";
+		String dbPassWord = "khcafe";
+		
+		
+		try {
+			Connection cc = DriverManager.getConnection(jdbcURL, dbUserName, dbPassWord);
+			Scanner sc = new Scanner(System.in);
+			
+			while(true) {
+				System.out.println("User ID 입력하세요.");
+				System.out.println("종료하고 싶다면 e 입력");
+				String input = sc.next();
+				System.out.println("User 이메일을 입력하세요");
+				String email = sc.next();
+				
+				//만약에 e를 입력했다면 
+				if("e".equalsIgnoreCase(input)) {
+					System.out.println("종료");
+					break; //break가 없으면 종료되지않음
+				}
+				
+				int userId = Integer.parseInt(input);
+				//select 문 출력하기
+				String sql = "select * from userinfo where user_id = ? and email = ?";
+				PreparedStatement st = cc.prepareStatement(sql);
+				st.setInt(1, userId);
+				st.setString(2, email);
+				ResultSet rs = st.executeQuery();
+				
+				//selectOne if
+				if(rs.next()) {
+					System.out.println("user ID : " + rs.getInt("user_id"));
+					System.out.println("userName : "+rs.getString("username"));
+					System.out.println("email : "+ rs.getString("email"));
+					System.out.println("Registration Date : " + rs.getDate("reg_date"));
+				}else {
+					//boolean ID or Email 하나가 일치하지 않는 경우 처리
+					boolean isTrue = (userId == rs.getInt("user_id")) ? true : false;
+					boolean emailTrue = email.equalsIgnoreCase(rs.getString("email"));
+					if(!isTrue && emailTrue) {
+						System.out.println("일치하지 않는 user ID입니다.");
+						System.out.println();
+     					}else if(isTrue&&!emailTrue) {
+						System.out.println("일치하지 않는 user email입니다.");
+					}else {
+						System.out.println("일치하는 user id 와 email을 찾을 수 없습니다.");
+						System.out.println();
+					}
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	public void selectAll() {
 		// 1. DB연결 url, username, password
